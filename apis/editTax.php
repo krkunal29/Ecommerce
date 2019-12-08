2dab8f46cb9ff276a461f5ca3748115e8972ce48
krkunal29 <krkunal29@gmail.com>
2019-12-08 14:38:37 +0530

    #  kunal update tax code

--- apis/editTax.php
+++ apis/editTax.php
@@ -10,11 +10,27 @@ if (isset($_POST['tax']) && isset($_POST['taxname']) && isset($_POST['taxId'])) 
     $sql          = "UPDATE taxmaster SET Tax='$tax'  ,Taxname='$taxname' WHERE TaxId = $taxId";
     $query        = mysqli_query($conn, $sql);
     $rowsAffected = mysqli_affected_rows($conn);
-    if ($rowsAffected > 0) {
-        $response = array(
-            'Message' => "Tax updated successfully",
-            'Responsecode' => 200
-        );
+    if ($rowsAffected == 1) {
+      $sql       = "SELECT * FROM taxmaster WHERE TaxId = $taxId";
+      $jobQuery1 = mysqli_query($conn, $sql);
+      if ($jobQuery1 != null) {
+          $academicAffected = mysqli_num_rows($jobQuery1);
+          if ($academicAffected > 0) {
+              $academicResults = mysqli_fetch_assoc($jobQuery1);
+              $records       = $academicResults;
+              $response        = array(
+                  'Message' => "Tax updated successfully",
+                  "Data" => $records,
+                  'Responsecode' => 200
+              );
+          } else {
+              $response = array(
+                  'Message' => "Please Add data first",
+                  "Data" => $records,
+                  'Responsecode' => 300
+              );
+          }
+      }
     } else {
         $response = array(
             "Message" => mysqli_error($conn) . "No data to change or user not present",
